d3108f8a382cabe90a5611360500a779979c9b29
guillaume.pomies82@gmail.com <guillaume.pomies82@gmail.com>
2022-04-12 15:51:28 +0200

    fini

--- premier_test_excel.py
+++ premier_test_excel.py
@@ -1,4 +1,7 @@
-from openpyxl import Workbook, load_workbook
+from time import sleep
+from tkinter import *
+from tkinter import ttk
+from openpyxl import*
 import pandas as pd
 read_file = pd.read_csv (r'C:/Users/Etudiant/Desktop/stage_excel/ExpDevis.csv',delimiter=";", encoding='latin1')
 read_file.to_excel (r'C:/Users/Etudiant/Desktop/stage_excel/ExpDevis.xlsx', index = None, header=True)
@@ -7,18 +10,38 @@ data= load_workbook('HYFR_DC-FC_2022 JZ.xlsx')
 ex = export.active
 da = data["Data"]
 colone=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ"]
-# ligne_ex=0
-# for row in ex:
-#     ligne_ex+=1
-# Date_Devis = (ex[colone[0]+str(ligne_ex)].value)
-# N_Devis = (ex[colone[1]+str(ligne_ex)].value)
-# Client=(ex[colone[2]+str(ligne_ex)].value)
-# ASM= (ex[colone[3]+str(ligne_ex)].value)
-# Montant= (ex[colone[4]+str(ligne_ex)].value)
-# Date=Date_Devis
-da.delete_rows(1)
-ligne_data=0
-for row in da:
+ligne_ex=0
+for row in ex:
+    ligne_ex+=1
+ligne_data=3
+while da[colone[6]+str(ligne_data)].data_type =="s":
     ligne_data+=1
-print(ligne_data)
-# print(Date_Devis,Date,N_Devis, Client,Montant,ASM)
+all_devis=[]
+for i in range(ligne_data): 
+    if type(da[colone[4]+str(i+1)].value)==int:
+        all_devis.append(da[colone[4]+str(i+1)].value)
+Date_Devis = (ex[colone[0]+str(ligne_ex)].value)
+bu_key=f"=IFERROR(INDEX(Tabelle2[BU],MATCH(tbl_DCFC[[#This Row],[Categorie]],CAT,0)),\"\")"
+cw=f"=IF({'A' + str(ligne_data)}<>\"\",\"S\"&TEXT(WEEKNUM({'A' + str(ligne_data)},21),\"00\"),\"\")"
+N_Devis = (ex[colone[1]+str(ligne_ex)].value)
+Client=(ex[colone[2]+str(ligne_ex)].value)
+ASM= (ex[colone[3]+str(ligne_ex)].value)
+Montant= int(ex[colone[4]+str(ligne_ex)].value)
+Date=Date_Devis
+Status=""
+if (ex[colone[5]+str(ligne_ex)].data_type)=="s":
+    Status='Validé'
+if Montant==0:
+    Status='Opportunité'
+indice_devis=all_devis.count(N_Devis)
+index=colone[indice_devis]
+Client_end=""
+Catégorie=""
+données=[Date,bu_key,cw,Date_Devis,N_Devis,index, Client,Client_end,Montant,ASM,Catégorie,Status]
+for i in range(len(données)):
+    da[colone[i]+str(ligne_data)]=données[i]
+data.save('HYFR_DC-FC_2022 JZ.xlsx')
+top=Tk()
+top.title("Excel")
+Label(top, text= "données ajoutées!").place(x=50,y=80)
+top.mainloop()
