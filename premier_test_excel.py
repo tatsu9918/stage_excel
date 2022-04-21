32f1bd1a3076a8a002b70389ca9881779bb124b9
guillaume.pomies82@gmail.com <guillaume.pomies82@gmail.com>
2022-04-21 16:09:10 +0200

    doublons fonctionnels

--- premier_test_excel.py
+++ premier_test_excel.py
@@ -2,6 +2,8 @@ from openpyxl import*
 from openpyxl.utils import*
 import pandas as pd
 import datetime as dt
+
+from requests import delete
 df = pd.read_excel('fac.xls', header=None)
 df.to_excel('fac.xlsx', index=False, header=False)
 read_file = pd.read_csv(
@@ -16,29 +18,29 @@ fa = facture.active
 da = data["Data"]
 colone = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA",
           "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ"]
-ligne_ex = 0
-for row in ex:
+ligne_ex = 2
+while ex[colone[0]+str(ligne_ex)].data_type == "s":
     ligne_ex += 1
 all_devis = []
 tmp_devis = ()
 
 ################################ données devis ################################
-for i in range(2, ligne_ex+1):
+for i in range(2, ligne_ex):
     ligne_data = 3
     while da[colone[6]+str(ligne_data)].data_type == "s":
         ligne_data += 1
     all_index = []
-    ind=0
+    ind = 0
     for ind in range(ligne_data):
         if type(da[colone[4]+str(ind+1)].value) == int:
             all_index.append(da[colone[4]+str(ind+1)].value)
     for devis in range(3, (ligne_data)):
-        tmp_devis = (dt.datetime.strftime((da[colone[3]+str(devis)].value),"%d/%m/%Y"), (da[colone[4] + str(devis)].value), (da[colone[6] + str(devis)].value), (da[colone[8] + str(devis)].value),
+        tmp_devis = (dt.datetime.strftime((da[colone[3]+str(devis)].value), "%d/%m/%Y"), (da[colone[4] + str(devis)].value), (da[colone[6] + str(devis)].value), (da[colone[8] + str(devis)].value),
                      (da[colone[9]+str(devis)].value), (da[colone[11] + str(devis)].value), (da[colone[17]+str(devis)].value), ((da[colone[18] + str(devis)].value)),
                      (da[colone[26]+str(devis)].value))
         all_devis.append(tmp_devis)
     date_devis = dt.datetime.strptime((ex[colone[0]+str(i)].value), "%d/%m/%Y")
-    bu_key = f"=IFERROR(INDEX(Tabelle2[BU],MATCH(tbl_DCFC[[#This Row],[Categorie]],CAT,0)),\"\")"
+    bu_key = f"=IFERROR(INDEX(Tabelle2[BU],MATCH([[#This Row],[Categorie]],CAT,0)),\"\")"
     cw = f"=IF({'D' + str(ligne_data)}<>\"\",\"S\"&TEXT(WEEKNUM({'D' + str(ligne_data)},21),\"00\"),\"\")"
     N_devis = (ex[colone[1]+str(i)].value)
     Client = (ex[colone[2]+str(i)].value)
@@ -98,7 +100,8 @@ for i in range(2, ligne_ex+1):
             ligne_fac += 1
         num_fac = {}
         i = 8
-        for i in range(i, ligne_fac):  # récup des num de facture pour les clés et les dates,montants et cliens
+        # récup des num de facture pour les clés et les dates,montants et cliens
+        for i in range(i, ligne_fac):
             num_fac[fa[colone[0]+str(i)].value] = (fa[colone[1]+str(i)].value, dt.datetime.strftime(
                 (fa[colone[4]+str(i)].value), "%d/%m/%Y"), fa[colone[11]+str(i)].value,)
             i += 1
@@ -110,20 +113,26 @@ for i in range(2, ligne_ex+1):
             n_facture = None
     else:
         date_réelle = None
-        cw_rev =None
+        cw_rev = None
         revenue_Year = None
         revenue_Month = None
-        dateliv_réelle =None
+        dateliv_réelle = None
         date_facture = None
         n_facture = None
     données_facture = [split2, date_réelle, cw_rev, revenue_Year,
                        revenue_Month, dateliv_réelle, n_facture, date_facture]
     données = données_devis+données_commande+données_facture
     ##############################  vérif devis  ################################
-    verif_devis = (dt.datetime.strftime(date_devis,"%d/%m/%Y"), N_devis, Client,float(Montant_devis),ASM, Status,
+    verif_devis = (dt.datetime.strftime(date_devis, "%d/%m/%Y"), N_devis, Client, float(Montant_devis), ASM, Status,
                    n_commande, montant_commande, dateliv_réelle)
     ################################ insersion excel ################################
     if verif_devis not in all_devis:
+        for i in range(len(all_devis)) :
+            if verif_devis[:5]==all_devis[i][:5] and (all_devis[i][8]==None or all_devis[i][6]==None):
+                da.delete_rows(i+3)
+                données[5]=colone[indice_devis-1]
+                ligne_data-=1 
+                print("doublons")     
         print(verif_devis)
         print(all_devis[len(all_devis)-1])
         # print("\n\n\n",all_devis)
