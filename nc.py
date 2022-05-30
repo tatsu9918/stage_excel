8f2da5486f77df3cd53a1bf5a7fb24635ca18b64
guillaume.pomies82@gmail.com <guillaume.pomies82@gmail.com>
2022-05-30 14:00:03 +0200

    plan d'action fonctionnel

--- nc.py
+++ nc.py
@@ -4,22 +4,24 @@ import datetime as dt
 colone = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA",
           "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ"]
 nom=input("FNC?\n")
-chemin=""
-if nom[7:9]=="01":
-    chemin="Y:/05-Quality/02-Non Conformités/03-Enregistrements_Non Conformités/01-En cours de traitement/01-Provenance fournisseur/"
-if nom[7:9]=="02":
-    chemin="Y:/05-Quality/02-Non Conformités/03-Enregistrements_Non Conformités/01-En cours de traitement/02-Detecté en Interne/"
-if nom[7:9]=="03":
-    chemin="Y:/05-Quality/02-Non Conformités/03-Enregistrements_Non Conformités/01-En cours de traitement/03-Détecté par le client/"
-feuille = load_workbook(chemin+nom+'.xlsx', data_only=True)
+
+# chemin=""
+# if nom[7:9]=="01":
+#     chemin="Y:/05-Quality/02-Non Conformités/03-Enregistrements_Non Conformités/01-En cours de traitement/01-Provenance fournisseur/"
+# if nom[7:9]=="02":
+#     chemin="Y:/05-Quality/02-Non Conformités/03-Enregistrements_Non Conformités/01-En cours de traitement/02-Detecté en Interne/"
+# if nom[7:9]=="03":
+#     chemin="Y:/05-Quality/02-Non Conformités/03-Enregistrements_Non Conformités/01-En cours de traitement/03-Détecté par le client/"
+feuille = load_workbook(nom+'.xlsx', data_only=True)
 monitoring = load_workbook('FR 1005-Monitoring NC & Action Plan-V001.xlsx')
-checklist = load_workbook('Y:/05-Quality/02-Non Conformités/Matrice_Process_NC.xlsm', keep_vba=True)
+checklist = load_workbook('checklist.xlsm', keep_vba=True)
 mo=monitoring["Suivi_FNC"]
 su=monitoring["Plan_Actions"]
 nc = feuille["Fiche de Non-Conformité"]
 ch=checklist["INT_DATA"]
 ch_ligne=mo_ligne=su_ligne=1
 all_fnc=[]
+all_pl=[]
 while ch[colone[1]+str(ch_ligne)].data_type == "s":
     ch_ligne += 1
 while mo[colone[0]+str(mo_ligne)].data_type == "s":
@@ -28,6 +30,9 @@ while su[colone[0]+str(su_ligne)].data_type == "s":
     su_ligne += 1
 for i in range(3,mo_ligne):
     all_fnc.append(mo[colone[0]+str(i)].value)
+for i in range (2,su_ligne):
+    all_pl.append([su[colone[0]+str(i)].value,su[colone[1]+str(i)].value])
+
 données=[]
 données.append(nc["l7"].value)
 données.append (dt.datetime.strftime((nc["AJ7"].value), "%d/%m/%Y"))
@@ -80,18 +85,21 @@ données.append(nc["au9"].value)
 plan_action=[]
 num_action=[21,22,23,62,63,64,65,66]
 for i in range (len(num_action)):
-    plan_action.append([nc["B"+str(num_action[i])].value,nom,"",nc["G"+str(num_action[i])].value,"",nc["AF"+str(num_action[i])].value,dt.datetime.strftime((nc["AP"+str(num_action[i])].value), "%d/%m/%Y")])
+    if nc["B"+str(num_action[i])].data_type!="n":
+        plan_action.append([nc["B"+str(num_action[i])].value,nom[:12],"",nc["G"+str(num_action[i])].value,"",nc["AF"+str(num_action[i])].value,dt.datetime.strftime((nc["AP"+str(num_action[i])].value), "%d/%m/%Y")])
 for i in range (len(plan_action)):
     for j in range (len(plan_action[i])):
-        su[colone[j]+str(su_ligne)]=plan_action[i][j]
+        if plan_action[i][:2] not in all_pl:
+            su[colone[j]+str(su_ligne)]=plan_action[i][j]
     su_ligne+=1
+#####insertion données
 for i in range (len(all_fnc)):
-    if nom == all_fnc[i]:
+    if nom[:12] == all_fnc[i]:
         mo_ligne=i+3
-    for i in range(len(données)):
+for i in range(len(données)):
         mo[colone[i]+str(mo_ligne)] = données[i]
-if nom not in all_fnc:
+if nom[:12] not in all_fnc:
     ch[colone[1]+str(ch_ligne)] = données[0]
 
 monitoring.save('FR 1005-Monitoring NC & Action Plan-V001.xlsx')
-checklist.save('Matrice_Process_NC.xlsm')
+checklist.save('checklist.xlsm')
